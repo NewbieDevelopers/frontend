9f2e5bbaebaa0f6d2ca3e2af64bd0faca58a4a73
Edsonjr1exe <edsonjr123info@gmail.com>
2024-09-20 21:37:47 -0300

    Adicionando arquivo inicial

---
+++ Projeto_Interdiciplinar/site/site.js
@@ -0,0 +1,22 @@
+// const ads = [
+//     '',
+//     'D:\Page 1 - Untitled - Opera 19_02_2024 15_29_11.png',
+//     'D:\Page 1 - Untitled - Opera 19_02_2024 15_29_03.png' 
+// ];
+
+// let currentAdIndex = 0;
+
+// function showAd(index) {
+//     const adImage = document.getElementById('adImage');
+//     adImage.src = ads[index];
+// }
+
+// function nextAd() {
+//     currentAdIndex = (currentAdIndex + 1) % ads.length;
+//     showAd(currentAdIndex);
+// }
+
+// function prevAd() {
+//     currentAdIndex = (currentAdIndex - 1 + ads.length) % ads.length;
+//     showAd(currentAdIndex);
+// }
